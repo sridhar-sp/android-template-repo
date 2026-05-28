4be8fd4bc9e728bd702d37c65a61c830d784c97c
sridhar-sp <sridharthechosenone@gmail.com>
2026-05-28 12:11:40 +0530

    Fix Typography dimensions

--- core/designsystem/src/main/java/com/droidstarter/designsystem/theme/Dimen.kt
+++ core/designsystem/src/main/java/com/droidstarter/designsystem/theme/Dimen.kt
@@ -8,10 +8,22 @@ import androidx.compose.ui.unit.sp
 interface AppDimen {
 
     val displayLarge: TextUnit
+
+    val displayMedium: TextUnit
+    val displaySmall: TextUnit
     val headlineLarge: TextUnit
+    val headlineMedium: TextUnit
+    val headlineSmall: TextUnit
     val titleLarge: TextUnit
+    val titleMedium: TextUnit
+    val titleSmall: TextUnit
     val bodyLarge: TextUnit
+    val bodyMedium: TextUnit
+    val bodySmall: TextUnit
     val labelLarge: TextUnit
+    val labelMedium: TextUnit
+    val labelSmall: TextUnit
+
 
     val microPadding: Dp
     val tinyPadding: Dp
@@ -29,23 +41,35 @@ interface AppDimen {
 
 class ScalableDimen(private val scaleFactor: Float = 1f) : AppDimen {
 
-    override val displayLarge: TextUnit get() = 57.sp * scaleFactor
-    override val headlineLarge: TextUnit get() = 32.sp * scaleFactor
-    override val titleLarge: TextUnit get() = 22.sp * scaleFactor
-    override val bodyLarge: TextUnit get() = 16.sp * scaleFactor
-    override val labelLarge: TextUnit get() = 14.sp * scaleFactor
-    override val microPadding: Dp get() = 2.dp * scaleFactor
-    override val tinyPadding: Dp get() = 4.dp * scaleFactor
-    override val smallPadding: Dp get() = 8.dp * scaleFactor
-    override val mediumPadding: Dp get() = 16.dp * scaleFactor
-    override val largePadding: Dp get() = 24.dp * scaleFactor
-    override val extraLargePadding: Dp get() = 32.dp * scaleFactor
-    override val smallElevation: Dp get() = 2.dp * scaleFactor
-    override val themeIconSize: Dp get() = 40.dp * scaleFactor
+    override val displayLarge: TextUnit = 57.sp * scaleFactor
+    override val displayMedium: TextUnit = 45.sp * scaleFactor
+    override val displaySmall: TextUnit = 36.sp * scaleFactor
+    override val headlineLarge: TextUnit = 32.sp * scaleFactor
+    override val headlineMedium: TextUnit = 28.sp * scaleFactor
+    override val headlineSmall: TextUnit = 24.sp * scaleFactor
+    override val titleLarge: TextUnit = 22.sp * scaleFactor
+    override val titleMedium: TextUnit = 16.sp * scaleFactor
+    override val titleSmall: TextUnit = 14.sp * scaleFactor
+    override val bodyLarge: TextUnit = 16.sp * scaleFactor
+    override val bodyMedium: TextUnit = 14.sp * scaleFactor
+    override val bodySmall: TextUnit = 12.sp * scaleFactor
+    override val labelLarge: TextUnit = 14.sp * scaleFactor
+    override val labelMedium: TextUnit = 12.sp * scaleFactor
+    override val labelSmall: TextUnit = 11.sp * scaleFactor
 
-    override val cardWidgetSize: Dp get() = 126.dp * scaleFactor
 
-    override val typographyWidgetDemoHeight: Dp get() = 250.dp * scaleFactor
+    override val microPadding: Dp = 2.dp * scaleFactor
+    override val tinyPadding: Dp = 4.dp * scaleFactor
+    override val smallPadding: Dp = 8.dp * scaleFactor
+    override val mediumPadding: Dp = 16.dp * scaleFactor
+    override val largePadding: Dp = 24.dp * scaleFactor
+    override val extraLargePadding: Dp = 32.dp * scaleFactor
+    override val smallElevation: Dp = 2.dp * scaleFactor
+    override val themeIconSize: Dp = 40.dp * scaleFactor
+
+    override val cardWidgetSize: Dp = 126.dp * scaleFactor
+
+    override val typographyWidgetDemoHeight: Dp = 250.dp * scaleFactor
 }
 
 val BigDimens = ScalableDimen(1.5f)
--- core/designsystem/src/main/java/com/droidstarter/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/com/droidstarter/designsystem/theme/Type.kt
@@ -28,71 +28,66 @@ private val fontFamily = FontFamily(
 class AppTypography(dimen: AppDimen) {
     private val defaultTypography = Typography()
 
-    private val displayLarge = dimen.displayLarge
-    private val headlineLarge = dimen.headlineLarge
-    private val titleLarge = dimen.titleLarge
-    private val bodyLarge = dimen.bodyLarge
-    private val labelLarge = dimen.labelLarge
-
-    private val mediumFontSizeScaleFactor = 1.25
-    private val smallFontSizeScaleFactor = mediumFontSizeScaleFactor * 2
-
     val typography = Typography(
         displayLarge = defaultTypography.displayLarge.copy(
-            fontFamily = fontFamily, fontSize = displayLarge
+            fontFamily = fontFamily,
+            fontSize = dimen.displayLarge
         ),
         displayMedium = defaultTypography.displayMedium.copy(
-            fontFamily = fontFamily, fontSize = displayLarge / mediumFontSizeScaleFactor
+            fontFamily = fontFamily,
+            fontSize = dimen.displayMedium
         ),
         displaySmall = defaultTypography.displaySmall.copy(
-            fontFamily = fontFamily, fontSize = displayLarge / smallFontSizeScaleFactor
+            fontFamily = fontFamily,
+            fontSize = dimen.displaySmall
         ),
         headlineLarge = defaultTypography.headlineLarge.copy(
             fontFamily = fontFamily,
-            fontSize = headlineLarge,
-            fontWeight = FontWeight.Bold,
+            fontSize = dimen.headlineLarge,
         ),
         headlineMedium = defaultTypography.headlineMedium.copy(
             fontFamily = fontFamily,
-            fontSize = headlineLarge / mediumFontSizeScaleFactor,
-            fontWeight = FontWeight.Bold,
+            fontSize = dimen.headlineMedium,
         ),
         headlineSmall = defaultTypography.headlineSmall.copy(
-            fontFamily = fontFamily, fontSize = headlineLarge / smallFontSizeScaleFactor
+            fontFamily = fontFamily,
+            fontSize = dimen.headlineSmall
         ),
         titleLarge = defaultTypography.titleLarge.copy(
             fontFamily = fontFamily,
-            fontSize = titleLarge,
-            fontWeight = FontWeight.SemiBold,
+            fontSize = dimen.titleLarge,
         ),
         titleMedium = defaultTypography.titleMedium.copy(
             fontFamily = fontFamily,
-            fontSize = titleLarge / mediumFontSizeScaleFactor,
+            fontSize = dimen.titleMedium,
         ),
         titleSmall = defaultTypography.titleSmall.copy(
-            fontFamily = fontFamily, fontSize = titleLarge / smallFontSizeScaleFactor
+            fontFamily = fontFamily,
+            fontSize = dimen.titleSmall
         ),
-        bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = fontFamily, fontSize = bodyLarge),
+        bodyLarge = defaultTypography.bodyLarge.copy(
+            fontFamily = fontFamily,
+            fontSize = dimen.bodyLarge
+        ),
         bodyMedium = defaultTypography.bodyMedium.copy(
-            fontFamily = fontFamily, fontSize = bodyLarge / mediumFontSizeScaleFactor, fontWeight = FontWeight.Normal
+            fontFamily = fontFamily,
+            fontSize = dimen.bodyMedium,
         ),
         bodySmall = defaultTypography.bodySmall.copy(
             fontFamily = fontFamily,
-            fontSize = bodyLarge / smallFontSizeScaleFactor,
-            fontWeight = FontWeight.Normal,
+            fontSize = dimen.bodySmall,
         ),
         labelLarge = defaultTypography.labelLarge.copy(
             fontFamily = fontFamily,
-            fontSize = labelLarge,
-            fontWeight = FontWeight.Normal,
+            fontSize = dimen.labelLarge,
         ),
         labelMedium = defaultTypography.labelMedium.copy(
             fontFamily = fontFamily,
-            fontSize = labelLarge / mediumFontSizeScaleFactor,
+            fontSize = dimen.labelMedium,
         ),
         labelSmall = defaultTypography.labelSmall.copy(
             fontFamily = fontFamily,
-            fontSize = labelLarge / smallFontSizeScaleFactor
+            fontSize = dimen.labelSmall
         ),
     )
 }
